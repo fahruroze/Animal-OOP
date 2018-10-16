a1b9994113acec2333680fbcf8327f13de4f7a75
fahruroze <fanyfahrurozi@gmail.com>
2018-10-16 22:10:10 +0700

    tugasoop

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal{
+
+	int jmlkaki;
+	String warna;
+	String jnsMakan;
+
+	public void breath(){
+		System.out.println("Animal Napas Woi..");
+	}
+	public void grow(){
+		System.out.println("Animal Cie Tumbuh..");
+	}	
+}
